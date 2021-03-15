5918d9b00da5d90856cdaebb221ccb357d149100
Mariwannaxsfzx <47078806+Mariwannaxsfzx@users.noreply.github.com>
2021-03-15 22:21:25 +0300

    Update train1.py

--- lab2/train1.py
+++ lab2/train1.py
@@ -58,7 +58,7 @@ def create_dataset(filenames, batch_size):
 
 def build_model():
   inputs = tf.keras.Input(shape=(RESIZE_TO, RESIZE_TO, 3))
-  x = tf.keras.applications.EfficientNetB0(include_top=False, input_tensor=inputs weights='imagenet')
+  x = tf.keras.applications.EfficientNetB0(include_top=False, input_tensor=inputs, weights='imagenet')
   x.trainable = False
   x = tf.keras.layers.GlobalAveragePooling2D()(x.output) 
   outputs = tf.keras.layers.Dense(NUM_CLASSES, activation=tf.keras.activations.softmax)(x)
