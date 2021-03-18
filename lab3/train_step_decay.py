f23957ee01be462e0f99370c609bac651a7e42e5
Mariwannaxsfzx <47078806+Mariwannaxsfzx@users.noreply.github.com>
2021-03-18 23:46:13 +0300

    Update train_step_decay.py

--- lab3/train_step_decay.py
+++ lab3/train_step_decay.py
@@ -66,9 +66,9 @@ def build_model():
 
 
 def step_decay(epoch):
-    initial_rate = 0.1
-    drop = 0.5
-    epochs_drop = 2
+    initial_rate = 0.01
+    drop = 0.6
+    epochs_drop = 5
     lr = initial_rate * pow(drop, (epoch//epochs_drop)) 
     print(f'{lr}')
     return lr
