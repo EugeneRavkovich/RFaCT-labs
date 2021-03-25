7e5a46afbcab685531be8b433ddf9db21c03b90c
Mariwannaxsfzx <47078806+Mariwannaxsfzx@users.noreply.github.com>
2021-03-26 00:49:28 +0300

    Update train.py

--- lab4/train.py
+++ lab4/train.py
@@ -55,7 +55,7 @@ def transforms(image):
   return aug_image
 
 def process_data(image, label):
-  aug_img = tf.numpy_function(func=transforms, inp=[image], Tout=tf.float32)
+  aug_img = tf.numpy_function(func=transforms, inp=[image], Tout=tf.uint8)
   return aug_img, label
   
   
