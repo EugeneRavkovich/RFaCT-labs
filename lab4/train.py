9d0721f97a92e72aaefc826ec607496ad2c66fa3
Mariwannaxsfzx <47078806+Mariwannaxsfzx@users.noreply.github.com>
2021-03-26 10:39:52 +0300

    Update train.py

--- lab4/train.py
+++ lab4/train.py
@@ -67,7 +67,6 @@ def create_dataset(filenames, batch_size):
   return tf.data.TFRecordDataset(filenames)\
     .map(parse_proto_example, num_parallel_calls=tf.data.AUTOTUNE)\
     .cache()\
-    .map(process_data)\
     .batch(batch_size)\
     .prefetch(tf.data.AUTOTUNE)
 
